47c4d9f5925218ab9789308e6253dddf72f2773d
Lucas Nunes <lucasnunes698.ln@gmail.com>
2023-05-24 23:34:40 -0300

    Add files via upload

--- ini.cs
+++ ini.cs
@@ -8,61 +8,104 @@ using System.Data.Odbc;
 using Microsoft.Win32;
 using FirebirdSql.Data.FirebirdClient;
 using System.Linq.Expressions;
+using System.Diagnostics;
 
 public class MainForm : Form
-{    
+{
+    private int StatusCheckBox;
     public MainForm()
     {
         InitializeComponents();
         Icon icon = new Icon("Rnv_Ico.ico");
         this.Icon = icon;
         this.BackColor = Color.FromArgb(54,56,114);
-        this.Size = new Size(1000, 800);
-        this.SizeChanged += Form1_SizeChanged;
+        this.Size = new Size(900, 500);
+        this.MinimumSize = new Size(900, 500);
+        this.MaximumSize = new Size(900, 500); 
+        this.MaximizeBox = false;
+        this.AutoScroll = true;
     }
-
     private void InitializeComponents()
     {
         try
         {
+            int rest = 0;
             int checkBoxWidth = 100;
             int checkBoxHeight = 20;
             int initialX = 50;
             int initialY = 150;
             int spacing = 10;
+
+            int BotaoWidth = 100;
+            int BotaoHeight = 20;
+            int BotaoinitialX = 345;
+            int BotaoinitialY = 70;
+
             string DiretorioDeExecução = Directory.GetCurrentDirectory();
             string diretorioPai = Path.Combine(DiretorioDeExecução, "..");
             string pastaProcurada = Path.Combine(diretorioPai, "dados");
-            string[] PastasEncontradas = Directory.GetDirectories(pastaProcurada);
+            string[] subpastas = Directory.GetDirectories(pastaProcurada);
 
-            for (int i = 0; i < PastasEncontradas.Length; i++)
+            // Título do formulário
+            Text = "Configuração de Novos Bancos de Dados e ODBC";
+            Icon = SystemIcons.Information;
+            int labelMargin = 5; // Margem entre o ícone e o Label
+            int labelWidth = 200; // Largura do Label
+            int labelHeight = SystemInformation.CaptionHeight; // Altura do Label igual à altura da barra de título
+            int labelX = Icon.Width + labelMargin; // Posição X do Label
+
+            // Criar o Label
+            Label label = new Label();
+            label.AutoSize = false;
+            label.Width = labelWidth;
+            label.Height = labelHeight;
+            label.Location = new Point(labelX, 0);
+            label.TextAlign = ContentAlignment.MiddleLeft;
+            label.Dock = DockStyle.None; // Desabilitar o dock para permitir ajuste manual
+            Controls.Add(label);
+
+            TextBox searchTextBox = new TextBox();
+            searchTextBox.Location = new Point(50, 35);
+            searchTextBox.Size = new Size(200, 50);
+            searchTextBox.ForeColor = Color.Black;
+            searchTextBox.Text = "Filtrar por nome";
+            searchTextBox.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
+            searchTextBox.TextChanged += new EventHandler(SearchTextBox_TextChanged);
+            Controls.Add(searchTextBox);
+
+            CheckBox atualizador = new CheckBox();
+            atualizador.Width = BotaoWidth;
+            atualizador.Height = BotaoHeight;
+            atualizador.AutoSize = true;
+            atualizador.Location = new Point(BotaoinitialX, BotaoinitialY);
+            atualizador.Text = "Atualizador de Banco SQL";
+            atualizador.BackColor = Color.White;
+            atualizador.CheckedChanged += Atualizador_CheckedChanged;
+            Controls.Add(atualizador);
+
+            for (int i = 0; i < subpastas.Length; i++)
             {
-                string folderPath = PastasEncontradas[i];
-                string folderName = Path.GetFileName(folderPath);
+                if (rest == 7)
+                {
+                    rest = 0;
+                    initialY += 45;
+                    initialX = 50;
+                    spacing = 10;
 
+                }
+                string folderPath = subpastas[i];
+                string folderName = Path.GetFileName(folderPath);
                 RadioButton checkBox = new RadioButton();
                 checkBox.BackColor = Color.White;
                 checkBox.Text = folderName;
-                checkBox.Location = new Point(initialX + (checkBoxWidth + spacing) * i, initialY);
+                checkBox.Location = new Point(initialX + (checkBoxWidth + spacing) * rest, initialY);
                 checkBox.Size = new Size(checkBoxWidth, checkBoxHeight);
                 checkBox.CheckedChanged += CheckBox_CheckedChanged;
                 Controls.Add(checkBox);
+                rest++;
             }
-
         }
-        catch (Exception ex)
-        {
-            MessageBox.Show("Ocorreu um erro: " + ex.Message);
-        }
-        TextBox searchTextBox = new TextBox();
-        searchTextBox.Location = new Point(50, 35);
-        searchTextBox.Size = new Size(200, 50);
-        searchTextBox.ForeColor = Color.Black;
-        searchTextBox.Text = "Filtrar por nome";
-        searchTextBox.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
-        searchTextBox.TextChanged += new EventHandler(SearchTextBox_TextChanged);
-
-        Controls.Add(searchTextBox);
+        catch (Exception ex){MessageBox.Show("Ocorreu um erro: " + ex.Message);}
     }
 
     private List<string> PegaCNPJ(object sender, EventArgs e)
@@ -90,10 +133,33 @@ public class MainForm : Form
                     }
                 }
             }
-
             connection.Close();
         }
         return cnpjResult;
+    }
+
+    private void Atualizador_CheckedChanged(object sender, EventArgs e)
+    {
+        CheckBox checkBox = (CheckBox)sender;
+        if (checkBox.Checked){StatusCheckBox = 1;}
+        else{StatusCheckBox = 0;}
+    }
+
+    private void CheckAtualizador()
+    {
+        string DiretorioDeExecuçãoDados = Directory.GetCurrentDirectory();
+        string diretorioPaiDados = Path.Combine(DiretorioDeExecuçãoDados, "..");
+        string diretorioAtualizador = Path.Combine(diretorioPaiDados, "Atualizador.exe");
+        string caminhoExe = diretorioAtualizador;
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = caminhoExe;
+        using (Process processo = new Process())
+        {
+            processo.StartInfo = startInfo;
+            processo.Start();
+            processo.WaitForExit();
+        }
+        MessageBox.Show("Banco Configurado com Sucesso!");
     }
 
     private void Criarini(object sender, EventArgs e)
@@ -101,29 +167,22 @@ public class MainForm : Form
             RadioButton checkBox = (RadioButton)sender;
             DialogResult resultado = MessageBox.Show("O Banco é unificado?", "Caixa de Diálogo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             int uniBanco = 0;
-            if (resultado == DialogResult.Yes)
-            {
-                uniBanco = 1;
-            }
-            else if (resultado == DialogResult.No)
-            {
-                uniBanco = 0;
-            }
+            if (resultado == DialogResult.Yes){uniBanco = 1;}
+            else if (resultado == DialogResult.No){uniBanco = 0;}
             string DiretorioDeExecução = Directory.GetCurrentDirectory();
             string diretorioPai = Path.Combine(DiretorioDeExecução, "..");
             string pastaProcurada = Path.Combine(diretorioPai, "dados");
             string[] diretorio = Directory.GetDirectories(pastaProcurada);
-
             string pastaDestino = @"\renovar";
             string NomeDoArquivo = "renovar.ini";
             string caminhoCompleto = Path.Combine(pastaDestino, NomeDoArquivo);
             List<string> cnpjResult = PegaCNPJ(sender, e);
+            List<string> DadosUni = new List<string>();
             for (int i = 1; i < 10; i++)
-            {
-                if (i <= cnpjResult.Count && !string.IsNullOrEmpty(cnpjResult[i - 1]))
-                {
-                    cnpjResult.Insert(i, "sem CNPJ");
-                }
+            {   
+                if(uniBanco == 0){DadosUni.Add("0"+i);}
+                else{DadosUni.Add("");}
+                if (i <= cnpjResult.Count && !string.IsNullOrEmpty(cnpjResult[i - 1])){cnpjResult.Insert(i, "sem CNPJ");}
             }
 
         string DadosDoArquivo = @"
@@ -210,11 +269,11 @@ PORT=
 EMPRESA=1
 [SISTEMA]
 DADOS01=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados.fdb
-DADOS02=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados02.fdb
-DADOS03=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados03.fdb
-DADOS04=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados04.fdb
-DADOS05=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados05.fdb
-DADOS06=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados06.fdb
+DADOS02=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados"+DadosUni[1]+".fdb"+@"
+DADOS03=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados"+DadosUni[2]+".fdb"+@"
+DADOS04=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados"+DadosUni[3]+".fdb"+@"
+DADOS05=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados"+DadosUni[4]+".fdb"+@"
+DADOS06=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados"+DadosUni[5]+".fdb"+@"
 DADOSEMP=C:\RENOVAR\DADOS\" + checkBox.Text + @"\DadosEmp.fdb
 DADOSREDE01=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados.fdb
 DADOSREDE02=C:\RENOVAR\DADOS\" + checkBox.Text + @"\Dados02.fdb
@@ -234,10 +293,7 @@ CNPJ01="+ cnpjResult[0] +"\n" +
 "CNPJ07=" + cnpjResult[6] + "\n" +
 "CNPJ08=" + cnpjResult[7] + "\n" +
 "CNPJ09=" + cnpjResult[8];
-        using (StreamWriter writer = new StreamWriter(caminhoCompleto))
-        {
-            writer.WriteLine(DadosDoArquivo);
-        }
+        using (StreamWriter writer = new StreamWriter(caminhoCompleto)){writer.WriteLine(DadosDoArquivo);}
     }
 
     private IEnumerable<string> BuscarArquivosDados(object sender, EventArgs e)
@@ -247,20 +303,16 @@ CNPJ01="+ cnpjResult[0] +"\n" +
         string diretorioPaiDados = Path.Combine(DiretorioDeExecuçãoDados, "..");
         string pastaDados = Path.Combine(diretorioPaiDados, "dados");
         string pastaComBanco = Path.Combine(pastaDados, checkBox.Text);
-        string[] arquivos = Directory.GetFiles(pastaComBanco, "*DADOS*")
-            .Where(arquivo => !Path.GetFileName(arquivo).Contains("DADOSEMP")).ToArray();
-
+        string[] arquivos = Directory.GetFiles(pastaComBanco, "*DADOS*").Where(arquivo => !Path.GetFileName(arquivo).Contains("DADOSEMP")).ToArray();
         return arquivos;
     }
 
     private void CheckBox_CheckedChanged(object sender, EventArgs e)
     {
         RadioButton checkBox = (RadioButton)sender;
-
         if (checkBox.Checked)
         {
-           Criarini(sender, e);
-
+            Criarini(sender, e);
             int i = 0;
             string pastaComBanco;
             IEnumerable<string> arquivosFiltrados = BuscarArquivosDados(sender, e);
@@ -288,7 +340,7 @@ CNPJ01="+ cnpjResult[0] +"\n" +
                     }
                     RegistryKey odbcKey = Registry.CurrentUser.OpenSubKey("Software\\ODBC\\ODBC.INI\\ODBC Data Sources", true);
                     odbcKey.SetValue(dsnName, driverName);
-                    if (odbcKey != null) { odbcKey.Close(); }
+                    if (odbcKey != null) { odbcKey.Close();}
                     RegistryKey dsnKey = Registry.CurrentUser.CreateSubKey("Software\\ODBC\\ODBC.INI\\" + dsnName);
                     if (dsnKey != null)
                     {
@@ -301,7 +353,8 @@ CNPJ01="+ cnpjResult[0] +"\n" +
                         dsnKey.Close();
                     }
                 }
-                MessageBox.Show("Banco Configurado com Sucesso!");
+                if (StatusCheckBox == 1){CheckAtualizador();}
+                else{MessageBox.Show("Banco Configurado com Sucesso!");}
             }
             catch (Exception ex)
             {
@@ -310,64 +363,15 @@ CNPJ01="+ cnpjResult[0] +"\n" +
         }
     }
 
-    private void Form1_SizeChanged(object sender, EventArgs e)
-    {
-        //ajuste dos RadioButton quando o tamanho da tela for alterado
-        AjustarRadioButtons();
-    }
-
-    private void AjustarRadioButtons()
-    {
-        int checkBoxWidth = 100; // Largura desejada para cada RadioButton
-        int checkBoxHeight = 20; // Altura desejada para cada RadioButton
-        int initialX = 50; // Posição inicial horizontal (X)
-        int initialY = 150; // Posição inicial vertical (Y)
-        int spacing = 10; // Espaçamento entre os RadioButton
-
-        // Calcula o número de colunas com base no tamanho atual da tela e a largura dos RadioButton
-        int numColumns = Math.Max((ClientSize.Width - initialX) / (checkBoxWidth + spacing), 1);
-
-        // Calcula a largura real de cada coluna considerando o espaçamento entre RadioButton
-        int columnWidth = (ClientSize.Width - initialX) / numColumns;
-
-        // Obtém a altura disponível com base no tamanho atual da tela e na posição vertical inicial
-        int availableHeight = ClientSize.Height - initialY;
-
-        // Calcula o número de linhas com base na altura disponível e na altura dos RadioButton
-        int numRows = Math.Max(availableHeight / (checkBoxHeight + spacing), 1);
-
-        // Atualiza as posições e tamanhos dos RadioButton
-        for (int i = 0; i < Controls.Count; i++)
-        {
-            Control control = Controls[i];
-            if (control is RadioButton radioButton)
-            {
-                int column = i % numColumns;
-                int row = i / numColumns;
-
-                int x = initialX + column * (columnWidth + spacing);
-                int y = initialY + row * (checkBoxHeight + spacing);
-
-                radioButton.Location = new Point(x, y);
-                radioButton.Size = new Size(columnWidth, checkBoxHeight);
-            }
-        }
-    }
-
     private void SearchTextBox_TextChanged(object sender, EventArgs e)
     {
         TextBox textBox = (TextBox)sender;
         string searchText = textBox.Text;
-
+        int rest = 0;
         if (!string.IsNullOrWhiteSpace(searchText))
         {
-            string termoDeBusca = searchText; 
-           
-
-            foreach (var control in Controls.OfType<RadioButton>().ToList())
-            {
-               Controls.Remove(control);
-            }
+            string termoDeBusca = searchText;            
+            foreach (var control in Controls.OfType<RadioButton>().ToList()){Controls.Remove(control);}
                 int checkBoxWidth = 100;
                 int checkBoxHeight = 20;
                 int initialX = 50;
@@ -378,36 +382,35 @@ CNPJ01="+ cnpjResult[0] +"\n" +
                 string pastaProcurada = Path.Combine(diretorioPai, "dados");
                 string[] diretorio = Directory.GetDirectories(pastaProcurada);
                 IEnumerable<string> pastasFiltradas = diretorio.Where(pasta => pasta.IndexOf(termoDeBusca, StringComparison.OrdinalIgnoreCase) >= 0);
-                int i = -1;
+                int i = 0;
                 foreach (string pasta in pastasFiltradas)
-                {
-                    
-                    i++;
-
+                {                
                     string folderPath = pasta.ToString();
-
                     string folderName = Path.GetFileName(folderPath);
-                    RadioButton checkBoxExistente = Controls.OfType<RadioButton>()
-                        .FirstOrDefault(control => string.Equals(termoDeBusca, folderName, StringComparison.OrdinalIgnoreCase)) as RadioButton;
-
-                if (checkBoxExistente == null)
+                    RadioButton checkBoxExistente = Controls.OfType<RadioButton>().FirstOrDefault(control => string.Equals(termoDeBusca, folderName, StringComparison.OrdinalIgnoreCase)) as RadioButton;
+                    if (checkBoxExistente == null)
                     {
-                        RadioButton checkBox = new RadioButton();
-                        checkBox.BackColor = Color.White;
-                        checkBox.Text = folderName;
-                        checkBox.Location = new Point(initialX + (checkBoxWidth + spacing) * i, initialY);
-                        checkBox.Size = new Size(checkBoxWidth, checkBoxHeight);
+                            RadioButton checkBox = new RadioButton();
+                            if (rest == 7)
+                            {
+                                rest=0;
+                                initialY += 45;
+                                initialX = 50;
+                                spacing = 10;
 
-
-                    checkBox.Size = new Size(checkBoxWidth, checkBoxHeight);
-                        checkBox.CheckedChanged += CheckBox_CheckedChanged;
-                        Controls.Add(checkBox);
-                }
-                    else
-                    {}
+                            }
+                            checkBox.BackColor = Color.White;
+                            checkBox.Text = folderName;
+                            checkBox.Location = new Point(initialX + (checkBoxWidth + spacing) * rest, initialY);
+                            checkBox.Size = new Size(checkBoxWidth, checkBoxHeight);
+                            checkBox.CheckedChanged += CheckBox_CheckedChanged;
+                            Controls.Add(checkBox);
+                    }
+                    rest++;
+                    i++;
                 }
         }
-        else 
+        else
         {
             foreach (var control in Controls.OfType<RadioButton>().ToList())
             {
@@ -423,19 +426,29 @@ CNPJ01="+ cnpjResult[0] +"\n" +
             string diretorioPai = Path.Combine(DiretorioDeExecução, "..");
             string pastaProcurada = Path.Combine(diretorioPai, "dados");
             string[] subpastas = Directory.GetDirectories(pastaProcurada);
-
             for (int i = 0; i < subpastas.Length; i++)
             {
                 string folderPath = subpastas[i];
                 string folderName = Path.GetFileName(folderPath);
 
                 RadioButton checkBox = new RadioButton();
+                if(rest == 7) 
+                {
+                    rest = 0;
+                    initialY += 45;
+                    checkBoxWidth = 100;
+                    checkBoxHeight = 20;
+                    initialX = 50;
+                    spacing = 10;
+
+                }
                 checkBox.BackColor = Color.White;
                 checkBox.Text = folderName;
-                checkBox.Location = new Point(initialX + (checkBoxWidth + spacing) * i, initialY);
+                checkBox.Location = new Point(initialX + (checkBoxWidth + spacing) * rest, initialY);
                 checkBox.Size = new Size(checkBoxWidth, checkBoxHeight);
                 checkBox.CheckedChanged += CheckBox_CheckedChanged;
                 Controls.Add(checkBox);
+                rest++;
             }
         }
 
